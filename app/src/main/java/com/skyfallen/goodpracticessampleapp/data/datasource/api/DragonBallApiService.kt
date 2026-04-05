5a11213a2fc56ae64ded19cb523d3234a2c05030
Miguel Ángel Montilla Pérez <miguelmontilla.1936@gmail.com>
2026-04-04 20:46:29 -0500

    Add parameter getCharacters endpoint

--- app/src/main/java/com/skyfallen/goodpracticessampleapp/data/datasource/api/DragonBallApiService.kt
+++ app/src/main/java/com/skyfallen/goodpracticessampleapp/data/datasource/api/DragonBallApiService.kt
@@ -1,9 +1,11 @@
 package com.skyfallen.goodpracticessampleapp.data.datasource.api
 
+import com.skyfallen.goodpracticessampleapp.model.CharactersResponse
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface DragonBallApiService {
 
     @GET("characters")
-    suspend fun getCharacters()
+    suspend fun getCharacters(@Query("page") page: Int): CharactersResponse
 }
