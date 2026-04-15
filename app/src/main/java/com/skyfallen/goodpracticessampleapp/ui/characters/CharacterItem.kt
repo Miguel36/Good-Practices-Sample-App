a854282f88bd26fd8ce6986b1e21c9b1bc4c100c
Miguel Ángel Montilla Pérez <miguelmontilla.1936@gmail.com>
2026-04-15 17:50:05 -0500

    Add Coil library and load character image

--- app/src/main/java/com/skyfallen/goodpracticessampleapp/ui/characters/CharacterItem.kt
+++ app/src/main/java/com/skyfallen/goodpracticessampleapp/ui/characters/CharacterItem.kt
@@ -2,7 +2,6 @@ package com.skyfallen.goodpracticessampleapp.ui.characters
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -15,9 +14,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil3.compose.AsyncImage
+import coil3.request.ImageRequest
+import coil3.request.crossfade
 import com.skyfallen.goodpracticessampleapp.model.Character
 
 @Composable
@@ -30,16 +33,22 @@ fun CharacterItem(character: Character) {
         elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
     ) {
         Column {
-            Box(Modifier.height(240.dp).fillMaxWidth().background(Color.DarkGray))
-            Column(Modifier.padding(8.dp)) {
-                Text(text = character.name, fontSize = 18.sp, fontWeight = FontWeight.SemiBold)
-                Text(text = character.race, fontSize = 14.sp)
-                Text(text = "Base Ki", fontSize = 14.sp, fontWeight = FontWeight.SemiBold)
-                Text(text = character.ki, fontSize = 14.sp)
+            AsyncImage(
+                modifier = Modifier.height(240.dp).fillMaxWidth().padding(top = 8.dp),
+                contentDescription = "Image from character",
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(character.image)
+                    .crossfade(true)
+                    .build()
+            )
+            Column(Modifier.background(Color.Black).fillMaxWidth().weight(1f).padding(8.dp)) {
+                Text(text = character.name, fontSize = 18.sp, fontWeight = FontWeight.SemiBold, color = Color.White)
+                Text(text = character.race, fontSize = 14.sp, color = Color.Yellow)
+                Text(text = "Base Ki", fontSize = 14.sp, fontWeight = FontWeight.SemiBold, color = Color.White)
+                Text(text = character.ki, fontSize = 14.sp, color = Color.Yellow)
                 Spacer(Modifier.height(8.dp))
-                Text(text = character.affiliation, fontSize = 16.sp, fontWeight = FontWeight.SemiBold)
+                Text(text = character.affiliation, fontSize = 16.sp, fontWeight = FontWeight.SemiBold, color = Color.White)
             }
-
         }
     }
 }
